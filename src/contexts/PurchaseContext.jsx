8c5dc86bbc980788c9caffa2a2ea4cddc27d780c
FILDSSS <kiangsak.r@japansystem.co.th>
2024-10-30 17:25:16 +0700

    update crate purchase data

--- src/contexts/PurchaseContext.jsx
+++ src/contexts/PurchaseContext.jsx
@@ -4,7 +4,9 @@ import axios from "../configs/axios";
 export const PurchaseContext = createContext();
 
 export default function PurchaseContextProvider({ children }) {
+
     const [purchaseData, setPurchaseData] = useState(null); 
+    
     const createPurchase = async () => {
         try {
             const response = await axios.post('/procure/add-procure', purchaseData); 
@@ -15,8 +17,26 @@ export default function PurchaseContextProvider({ children }) {
             throw new Error('Failed to create order');
         }
     };
+
+    const searchPurchaseData = async (orderNo) => {
+        try {
+            const response = await axios.post("/procure/search-procure", { Order_No: orderNo});
+            
+            // Check if the response contains valid procure data
+            if (response.data && response.data.data && response.data.data.procure) {
+                setPurchaseData(response.data.data.procure); // Correctly set purchase data from procure
+                return true; 
+            } else {
+                return false; 
+            }
+        } catch (error) {
+            console.error("Error fetching order data:", error);
+            return false; 
+        }
+    };
+
     return (
-        <PurchaseContext.Provider value={{purchaseData, setPurchaseData, createPurchase,}}>
+        <PurchaseContext.Provider value={{purchaseData, setPurchaseData, createPurchase,searchPurchaseData,}}>
             {children}
         </PurchaseContext.Provider>
     );
